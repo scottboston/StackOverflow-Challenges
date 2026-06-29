ed5a53827337528db1bb92bac1fabdfec32e2214
Scott Boston <scott.boston@yahoo.com>
2026-06-28 20:23:38 -0500

    Black this file

--- Challenge20.py
+++ Challenge20.py
@@ -1,14 +1,24 @@
 import pandas as pd
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    scenarios = {'Scenario one':  r'C:\Users\scott\PycharmProjects\StackOverflow-Challenges\data\challenge_20_s1.txt',
-                 'Scenario two':  r'C:\Users\scott\PycharmProjects\StackOverflow-Challenges\data\challenge_20_s2.txt',
-                 'Scenario three': r'C:\Users\scott\PycharmProjects\StackOverflow-Challenges\data\challenge_20_s3.txt'}
+    scenarios = {
+        "Scenario one": r"C:\Users\scott\PycharmProjects\StackOverflow-Challenges\data\challenge_20_s1.txt",
+        "Scenario two": r"C:\Users\scott\PycharmProjects\StackOverflow-Challenges\data\challenge_20_s2.txt",
+        "Scenario three": r"C:\Users\scott\PycharmProjects\StackOverflow-Challenges\data\challenge_20_s3.txt",
+    }
     for scenario, file in scenarios.items():
         print(f"\n{scenario}:\n")
-        df = pd.read_csv(file, sep=': ', engine='python', names=['name', 'guest', 'flavor'])
-        df_out = df.groupby('flavor')['guest'].agg(['sum', 'count']).sum(axis=1).mul(1.5).floordiv(-9).mul(-1)
+        df = pd.read_csv(
+            file, sep=": ", engine="python", names=["name", "guests", "flavor"]
+        )
+        df_out = (
+            df.groupby(by="flavor", sort=False)["guests"]
+            .agg(["sum", "count"])
+            .sum(axis=1)
+            .mul(1.5)
+            .floordiv(-9)
+            .mul(-1)
+        )
         for idx, value in df_out.items():
             print(f"  • {idx}: {value:.0f} tub{'s'[:int(value)^1]} ")
-
